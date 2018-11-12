28241eace0bc4bb6c5db648af9706983ca7d76e5
王立巍 <highter@outlook.com>
2018-11-13 00:40:04 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -11,6 +11,8 @@ date
 echo "Domain: ${DOMAIN}, Record type: ${RECORD_TYPE}, Record name: ${RECORD_NAME}"
 echo "Begin ddns update."
 RECORD_CONTENT=$(/sbin/ifconfig | grep 'inet addr' | grep -v '127.0.0' | awk '{print $2}' | awk -F: '{print $2}')
+# another way
+# RECORD_CONTENT=$(curl ip.sb)
 echo "Current IP: ${RECORD_CONTENT}"
 DOMAIN_ID=$(curl --insecure -X GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}" -H "X-Auth-Email: ${EMAIL}" -H "X-Auth-Key: ${TOKEN}" 2>/dev/null | awk -F\" '{print $6}')
 RECORD_ID=$(curl --insecure -X GET "https://api.cloudflare.com/client/v4/zones/${DOMAIN_ID}/dns_records?type=${RECORD_TYPE}&name=${RECORD_NAME}" -H "X-Auth-Email: ${EMAIL}" -H "X-Auth-Key: ${TOKEN}" 2>/dev/null | awk -F\" '{print $6}')
